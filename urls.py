17d3014c04c0dc83eec6382b4849d61cb334a5f3
Ermiyas <ermi1223b@gmail.com>
2023-01-13 13:25:46 +0300

    user registration

--- urls.py
+++ urls.py
@@ -4,6 +4,13 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 
 
+# from rest_framework import routers
+# from django.conf.urls import include
+# from django.urls import path
+# from rest_framework_simplejwt.views import TokenRefreshView
+# from views import (MyObtainTokenPairView, RegisterView)
+#                     
+
 urlpatterns = [
     path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
