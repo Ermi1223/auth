4207e05ecf15148143d74666803d58dfb2c2a570
Esubalew <esubalew.a2009@gmail.com>
2023-02-09 10:09:45 -0500

    allow login by username or email

--- auth_backends.py
+++ auth_backends.py
@@ -5,11 +5,16 @@ class EmailBackend(BaseBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         UserModel = get_user_model()
         try:
-            user = UserModel.objects.get(email=username)
+            user = UserModel.objects.get(username=username)
             if user.check_password(password):
                 return user
         except UserModel.DoesNotExist:
-            return None
+            try:
+                user = UserModel.objects.get(email=username)
+                if user.check_password(password):
+                    return user
+            except UserModel.DoesNotExist:
+                return None
 
     def get_user(self, user_id):
         UserModel = get_user_model()
