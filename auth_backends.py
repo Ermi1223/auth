21ab4bb2c8d2df9ae7bca568c7598a84560812dc
Esubalew <esubalew.a2009@gmail.com>
2023-02-13 04:16:57 -0500

    improvements on login error response

--- auth_backends.py
+++ auth_backends.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth import get_user_model
+from rest_framework.exceptions import ValidationError
 
 class EmailBackend(BaseBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
@@ -8,13 +9,18 @@ class EmailBackend(BaseBackend):
             user = UserModel.objects.get(username=username)
             if user.check_password(password):
                 return user
+            else:
+                raise ValidationError({'detail': 'Incorrect password.'})
         except UserModel.DoesNotExist:
             try:
                 user = UserModel.objects.get(email=username)
                 if user.check_password(password):
                     return user
+                else:
+                    raise ValidationError({'detail': 'Incorrect password.'})
+
             except UserModel.DoesNotExist:
-                return None
+                raise ValidationError({'detail': 'User not found.'})
 
     def get_user(self, user_id):
         UserModel = get_user_model()
